5214b04f5b71dc8a5404decfeec1f23159fefabe
Michael Bestas <mkbestas@lineageos.org>
2025-01-25 12:37:38 +0200

    fogos: Import blobs & firmware from U1UGS34.23-110-23-4
    Change-Id: I3e4ae269ff5083235cd6961fb273ed427cf0b227

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),fogos)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,73c13059616831863cdc604fe09d140eb89cdfe3)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,0c208b1c7dadcbcc40ed886f9ecb18d062f37a5f)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,46ba86f287f382e6674994d6d13ed83590da9874)
+$(call add-radio-file-sha1-checked,radio/dsp.img,6be03a523b4a75f993c9cf3f14537feaf6f2e57e)
+$(call add-radio-file-sha1-checked,radio/fsg.img,00ba7613d85008a1dcf0fe0b506419e333a570f8)
+$(call add-radio-file-sha1-checked,radio/hyp.img,23fb6c4bc130c9affe877333f936e55881946dc1)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,e1b70e9e9d2160835dab79f9422868965abc549e)
+$(call add-radio-file-sha1-checked,radio/logo.img,b3ee2745ee6a75b62a19caa63cdb5043710df5ac)
+$(call add-radio-file-sha1-checked,radio/modem.img,791814149b1535ed6c1b076f7aa2647e05810624)
+$(call add-radio-file-sha1-checked,radio/prov.img,1aad021746e1cc8dfaf88c214bf553916f06847a)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,e9fa7d22eb00ccde0a2310364fe8619192e9a7bf)
+$(call add-radio-file-sha1-checked,radio/rpm.img,f3bc9671236188790c64c00841f803d5377f5cef)
+$(call add-radio-file-sha1-checked,radio/storsec.img,8f3317a958849f1832780c94fa8a7d583b841930)
+$(call add-radio-file-sha1-checked,radio/tz.img,dbe9bece36f3507bceee76c7ac47bee2bcc04058)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,f7c539921467125d27f7a4a3d72b0dfe3fad88f9)
+$(call add-radio-file-sha1-checked,radio/xbl.img,679277e00977428981a755a40005bc9e83dd5fbd)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,5d233ff90d3e5da2c9417512e34dcd448862a88c)
+
+endif
